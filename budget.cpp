09cf7acf9eeb92eb8cb27b19943e3c1298c33deb
Nikita Litvin <deltaidea@derpy.ru>
2015-06-05 02:32:51 +0500

    Use vector with possible action choices instead of `switch`
    This allows to store action titles and (result messages and actions) in the same place,
    and also the list is now flexible enough to allow to just add items and/or rearrange them.

--- budget.cpp
+++ budget.cpp
@@ -8,7 +8,19 @@
 #include <cmath>
 #include <ctime>
 #include <limits>
+#include <functional>
+#include <vector>
 using namespace std;
+
+// http://www.codeguru.com/cpp/cpp/cpp_mfc/stl/article.php/c4027/C-Tutorial-A-Beginners-Guide-to-stdvector-Part-1.htm
+// http://stackoverflow.com/questions/6601930/c11-lambda-as-member-variable
+// http://stackoverflow.com/questions/8906545/how-to-initialize-a-vector-in-c
+// http://stackoverflow.com/questions/11516657/c-structure-initialization
+struct ActionChoice {
+	string title;
+	string resultMessage;
+	function<void()> action;
+};
 
 string promptUsername()
 {
@@ -44,12 +56,116 @@ int main()
 	string name;
 	string sixChoice; //choice for option 6
 	double score;
+	vector<ActionChoice> actionChoices;
 
 	srand(time(0)*time(0)*time(0)*time(0)); //init rand
 
 	schoolBudget = rand() % 100000 + 50000;
 
 	name = promptUsername();
+
+	actionChoices = {
+		{
+			"Improve the toilets ($20000).",
+			"You've improved the toilets.",
+			[&]() {
+				schoolBudget -= 20000;
+				reputation += 0.4;
+			}
+		},
+		{
+			"Put flower pots for more beauty ($15000).",
+			"You put flower pots around the school.",
+			[&]() {
+				schoolBudget -= 15000;
+				reputation += 0.1;
+			}
+		},
+		{
+			"Dress up day ($1000). Chance of increasing or decreasing reputation.",
+			"You put up a fabulous dress up day.",
+			[&]() {
+				rng = rand() % 4;
+				schoolBudget -= 1000;
+				cout << "That went ";
+				if(rng>=2) {
+					cout << "alright. You gained 0.1 reputation.\n";
+					reputation += 0.1;
+				}
+				else {
+					cout << "not very well. You lost 0.1 reputation.\n";
+					reputation -= 0.1;
+				}
+				reputation += rng;
+			}
+		},
+		{
+			"Food sale (+$10000). Will lower population in exchange for money boost.",
+			"You ask the students to make food for sale.",
+			[&]() {
+				cout << "You've lost 5 population but you gained some money!\n";
+				schoolPop -= 5;
+				schoolBudget += 10000;
+			}
+		},
+		{
+			"Introduction to school ($5000). Chance of increasing or decreasing population.",
+			"You introduce more people to the school.",
+			[&]() {
+				schoolBudget -= 5000;
+				cout << "The event went ";
+				rng = rand() % 4;
+				if(rng>=2) {
+					rng = 10;
+					cout << "alright. You gained 10 students! \n";
+				}
+				else {
+					rng = -10;
+					cout << "terribly wrong. Students are leaving! You've lost 10 students.\n";
+				}
+			}
+		},
+		{
+			"Improve sound systems ($20000). Will lower reputation in exchange for population.",
+			"You install new sound systems for the school.",
+			[&]() {
+				cout << "How tragic: During testing of the sound systems, some students have their eardrums severely injured.\n";
+				cout << "Please pay some respects: ";
+				cin >> sixChoice;
+				if(sixChoice == "F","f") {
+					reputation -= 0.1;
+				}
+				else {
+					reputation -= 0.2;
+					schoolPop += 5;
+				}
+			}
+		},
+		{
+			"Automagic water fountains ($10000). Make the students happier?",
+			"You install new automatic water fountains.",
+			[&]() {
+				cout << "However, none of them work.\n";
+				rng = rand() % 1 + 2;
+				if(rng==2) {
+					cout << "It flooded the school! You have to pay lots of money to fix it now.\n";
+					schoolBudget -= 20000;
+				}
+				else {
+					cout << "Thank god that it didn't flood or break. They just don't work.\n";
+				}
+				cout << "Oh well, back to manual water fountains it is.\n";
+			}
+		},
+		{
+			"Parasols in cafeteria ($15000). Will lower population in exchange for reputation.",
+			"These new parasols look great! Everyone loves you for it. However some students have left because they are allergic to yellow umbrellas.",
+			[&]() {
+				schoolPop -= 5;
+				reputation += 0.15;
+			}
+		},
+	};
 
 	for(month = 1; month <= 12; month++) {
 
@@ -71,16 +187,15 @@ int main()
 		cout << "\n";
 		cout << "Right now you have: " << schoolBudget << " dollars.\n";
 		cout << "...\n";
-			cout << "1. Improve the toilets ($20000)\n";
-			cout << "2. Put flower pots for more beauty ($15000)\n";
-			cout << "3. Dress up day ($1000). Chance of increasing or decreasing reputation.\n";
-			cout << "4. Food sale (+$10000). Will lower population in exchange for money boost.\n";
-			cout << "5. Introduction to school ($5000). Chance of increasing or decreasing population.\n";
-			cout << "6. Improve sound systems ($20000). Will lower reputation in exchange for population.\n";
-			cout << "7. Automagic water fountains ($10000). Make the students happier?";
-			cout << "8. Parasols in cafeteria ($15000). Will lower population in exchange for reputation";
+
+		// http://stackoverflow.com/questions/12702561/c-iterate-through-vector-using-for-loop
+		int i = 1;
+		for (auto &choice : actionChoices) {
+			cout << i++ << ". " << choice.title << "\n";
+		}
 
 		cout << "What would you like to do this month? ";
+
 		while( ! ( cin >> choice ) ) {
 			cout << "Invalid input. Try again: ";
 			cin.clear();
@@ -88,89 +203,15 @@ int main()
 		}
 
 		cout << "Alright... As you wish.\n\n";
-		switch(choice) {
-			case 1:	// Is this python?
-				cout << "You've improved the toilets.\n";
-				schoolBudget -= 20000;
-				reputation += 0.4;
-				break;
-			case 2:
-				cout << "You put flower pots around the school.\n";
-				schoolBudget -= 15000;
-				reputation += 0.1;
-				break;
-			case 3:
-				cout << "You put up a fabulous dress up day.\n";
-				schoolBudget -= 1000;
-				rng = rand() % 4; // There we go fixed it
-				cout << "That went ";
-				if(rng>=2) {
-					cout << "alright. You gained 0.1 reputation.\n\n";
-					reputation += 0.1;
-				}
-				else {
-					cout << "Not very well. You lost 0.1 reputation.\n\n";
-					reputation -= 0.1;
-				}
-				reputation += rng;
-				break;
-			case 4:
-				cout << "You ask the students to make food for sale.\n";
-				cout << "You've lost 5 population but you gained some money!\n";
-				schoolPop -= 5;
-				schoolBudget += 10000;
-				break;
-			case 5:
-				cout << "You introduce more people to the school.";
-				schoolBudget -= 5000;
-				cout << "The event went ";
-				rng = rand() % 4;
-				if(rng>=2) {
-					rng = 10;
-					cout << "alright. You gained 10 students! \n";
-				}
-				else {
-					rng = -10;
-					cout << "terribly wrong. Students are leaving! You've lost 10 students.\n";
-				}
-				break;
-			case 6:
-				schoolBudget -= 20000;
-				cout << "You install new sound systems for the school.";
-				cout << "How tragic: During testing of the sound systems, some students have their eardrums severely injured.\n";
-				cout << "Please pay some respects: ";
-				cin >> sixChoice;
-				if(sixChoice == "F","f") {
-					reputation -= 0.1;
-				}
-				else {
-					reputation -= 0.2;
-					schoolPop += 5;
-				}
-				break;
-			case 7:
-				schoolBudget -= 10000;
-				cout << "You install new automatic water fountains.\n";
-				cout << "However, none of them work.";
-				rng = rand() % 1 + 2;
-				if(rng==2) {
-					cout << "It flooded the school! You have to pay lots of money to fix it now.\n";
-					schoolBudget -= 20000;
-				}
-				else {
-					cout << "Thank god that it didn't flood or break. They just don't work.\n";
-				}
-				cout << "Oh well, back to manual water fountains it is.\n";
-				break;
-			case 8:
-				schoolBudget -= 15000;
-				cout << "These new parasols look great! Everyone loves you for it. However some students have left because they are allergic to yellow umbrellas.\n";
-				schoolPop -= 5;
-				reputation += 0.15;
-				break;
-			default:
-				cout << "Administration didn't understand what you mean. They spent the whole month on seminars. (-$1000)";
-				schoolBudget -= 1000;
+
+		choice -= 1;
+
+		if (choice >= 0 && choice < actionChoices.size()) {
+			cout << actionChoices[choice].resultMessage << "\n";
+			actionChoices[choice].action();
+		} else {
+			cout << "Administration didn't understand what you mean. They spent the whole month on seminars. (-$1000)\n";
+			schoolBudget -= 1000;
 		}
 
 	}
