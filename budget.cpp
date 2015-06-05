db3b362ed380a8f3eb97dbadca36fe0efd09dec7
suclearnub <suclearnub00@gmail.com>
2015-06-05 14:20:43 +0800

    semicolons
    i swear to god

--- budget.cpp
+++ budget.cpp
@@ -187,8 +187,8 @@ int main()
 		},
 
 		{
-			"Hire a teacher of unknown quality for an unknown cost.";
-			"You hired a teacher - but it'll be some time before you know how well he performs.";
+			"Hire a teacher of unknown quality for an unknown cost.",
+			"You hired a teacher - but it'll be some time before you know how well he performs.",
 			[&]() {
 				if(teacherQualityDescription=="good") {
 					teacherQualityDescription="loved";
